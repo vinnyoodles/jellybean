28f056c9ef4a8951e6eeecc7bb354fcacec49f87
Vincent Le <vinnyoodles@gmail.com>
2023-05-11 22:17:04 -0700

    add test case for matrix multiplication

--- test/controllers/api/v1/question_controller_test.rb
+++ test/controllers/api/v1/question_controller_test.rb
@@ -5,4 +5,10 @@ class Api::V1::QuestionControllerTest < ActionDispatch::IntegrationTest
     get api_v1_question_create_url
     assert_response :success
   end
+
+  test "should calculate dot product" do
+    controller = Api::V1::QuestionController.new
+
+    assert controller.send(:calculate_similarity, [1,2], [3, 4]) == 11
+  end
 end
