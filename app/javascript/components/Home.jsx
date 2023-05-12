209258bd55b174459a9d0ea7f5272a48771d82f1
Vincent Le <vinnyoodles@gmail.com>
2023-05-11 18:22:34 -0700

    add basic react form component

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
-export default () => (
-    <div>Ask a question</div>
-);
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            question: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({question: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        alert('Submitted question: ' + this.state.question);
+      }
+
+    render() {
+        return (
+            <div className="pt-5 w-50 mx-auto">
+                <form className="d-flex flex-column" onSubmit={this.handleSubmit}>
+                    <label>Ask a question:</label>
+                    <textarea type="text" id="question" value={this.state.question} onChange={this.handleChange}>
+                    </textarea>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
+};
